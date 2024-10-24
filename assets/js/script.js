5f078c8fe99b86a560fdd8504aaa7c1f15adfd71
linokubemba <emmanuel.lino-kubemba@outlook.fr>
2024-10-24 11:21:00 +0200

    fix: hide the apiKey

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,28 @@
-const apiKey = '';
+import { apiKey } from './config.js';
 
-const url = 'https://www.googleapis.com/books/v1/volumes?q=books';
+const url = 'https://www.googleapis.com/books/v1/volumes?q=';
 
-async function fetchData() {
+// async function fetchData() {
 
-    try {
-        const response = await fetch("");
-        console.log(response);
+//     try {
+//         const response = await fetch("");
+//         console.log(response);
 
-        const data = await res.json();
-        console.table(data);
-        title.textContent = data.title;
-    } catch (error) {
-        console.log("Erreur lors de la récupération des données", error);
-    }
-}
+//         const data = await res.json();
+//         console.table(data);
+//         title.textContent = data.title;
+//     } catch (error) {
+//         console.log("Erreur lors de la récupération des données", error);
+//     }
+// }
 
-// const theme = 'science fiction'; // remplacer par ce que l'utilisateur a tapé
+const theme = 'science fiction'; // remplacer par ce que l'utilisateur a tapé
 // const apiKey = ''; // Remplace par ta vraie clé API
 // const url = `https://www.googleapis.com/books/v1/volumes?q=subject:${theme}&key=`;
 
-// fetch(url)
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log(data.items); // Afficher ou traiter les livres récupéré
-//   })
-//   .catch(error => console.error('Erreur:', error));
+fetch(url + `subject:${theme}&key=` + apiKey)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.items); // Afficher ou traiter les livres récupéré
+  })
+  .catch(error => console.error('Erreur:', error));
