dab798bf5366e3b15e843f71adc976025be8975e
Quentin <quentin.degli-esposti@coexya.eu>
2024-10-25 12:06:51 +0200

    fix : display book on storypage + feat : searchbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,21 +5,13 @@ import { DetailBook } from './detailBook.js';
 const book = new DetailBook();
 book.getBooks();
 
-
-
-// const theme = 'science fiction'; // remplacer par ce que l'utilisateur a tapé
-// // const apiKey = ''; // Remplace par ta vraie clé API
-// // const url = `https://www.googleapis.com/books/v1/volumes?q=subject:${theme}&key=`;
-
-
-
-
-
-
-
+document.getElementById('searchButton').addEventListener('click', () => {
+  const searchQuery = document.getElementById('searchInput').value;
+  searchBooks(searchQuery);
+});
 
 // const url = `https://www.googleapis.com/books/v1/volumes?q=books&key=${apiKey}`;
-const url = `https://www.googleapis.com/books/v1/volumes?q=roman+pour+enfant&key=${apiKey}`;
+const url = `https://www.googleapis.com/books/v1/volumes?q=subject:juvenile+fiction&key=${apiKey}&maxResults=20`;
 
 async function fetchData() {
 
@@ -46,6 +38,8 @@ fetchData();
 function displayBooks(books) {
   const categoryWrapper = document.getElementById('categoryWrapper2');
 
+  categoryWrapper.innerHTML = '';
+
   books.forEach(book => {
       const bookCard = document.createElement('div');
 
@@ -63,12 +57,12 @@ function displayBooks(books) {
 
       bookCard.innerHTML = `
           <a href="#">
-          <img src="${bookImage}" style="width: 100%; height: 193px; object-fit: cover; border-top-left-radius: 5px; border-top-right-radius: 5px;" alt="${bookTitle}">
-          <div class="musicPlayerStoryBookText">
-              <h3>${bookTitle}</h3>
-              <p>${bookDescription}</p>
-              <span>${bookAuthor}</span>
-          </div>
+            <img src="${bookImage}" style="width: 100%; height: 193px; object-fit: cover; border-top-left-radius: 5px; border-top-right-radius: 5px;" alt="${bookTitle}">
+            <div class="musicPlayerStoryBookText">
+                <h3>${bookTitle}</h3>
+                <p>${bookDescription}</p>
+                <span>${bookAuthor}</span>
+            </div>
           </a>
       `;
 
@@ -83,6 +77,27 @@ function truncateByWords(text, wordLimit) {
   }
   return text;
 }
+
+async function searchBooks(query) {
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}&maxResults=10`;
+  
+
+  try {
+      const response = await fetch(url);
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      displayBooks(data.items);
+      console.log(data)
+  } catch (error) {
+      console.log("Erreur lors de la récupération des données", error);
+  }
+}
+
+
+
+// 
 
 // const theme = 'science fiction'; // remplacer par ce que l'utilisateur a tapé
 // // const apiKey = ''; // Remplace par ta vraie clé API
